58d317e1f81d0d320de14f52a7f22b8391b3872b
Matheus <matheusbrito20015@gmail.com>
2024-12-13 21:18:40 -0300

    entities departmentand seller

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,7 +1,17 @@
 package app;
 
+import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Department obj = new Department(1,"Books");
+
+        Seller seller = new Seller(21, "Maik", "mail@gmail.com", new Date(),4000.0, obj);
+
+        System.out.println(seller);
     }
 }
